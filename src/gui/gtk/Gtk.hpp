544d2074e192476316304c3ad426855fc428c779
truNEET <gtorrent.gentoo@gmail.com>
2014-07-19 00:45:33 -0500

    Replace Gtk::Main object with static initialization

--- src/gui/gtk/Gtk.hpp
+++ src/gui/gtk/Gtk.hpp
@@ -1,22 +1,15 @@
 #pragma once
 
-#include <gui/core/Base.hpp>
 #include "GtkMainWindow.hpp"
 #include <gtkmm/settings.h>
 #include <gtkmm/main.h>
 #include <memory>
 
-class GuiGtk : public GuiBase
+class GuiGtk
 {
 private:
-	Gtk::Main *m_gtkmain;
 	GtkMainWindow *mainWindow;
 public:
 	GuiGtk(int argc, char **argv);
 	~GuiGtk();
-
-	// Public Members & Functions
-
-	void update();
 };
-
